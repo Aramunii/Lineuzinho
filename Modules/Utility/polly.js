044da1b0613b6fa9606a7ca84113b55051e5ac39
Aramuni <warley.maer@gmail.com>
2021-12-10 09:39:14 -0300

    polly config

--- Modules/Utility/polly.js
+++ Modules/Utility/polly.js
@@ -12,6 +12,7 @@ methods.getPolly = async function getPolly(client, message, text) {
     AWS.config.loadFromPath('Modules/Utility/config.json');
     var splited = text.split(' ');
     var language = splited[0];
+    var typeSpeak = splited[1];
 
     splited = splited.filter(function (item) {
         return item !== language
@@ -24,7 +25,9 @@ methods.getPolly = async function getPolly(client, message, text) {
         { name: 'au', voice: 'Olivia', engine: 'neural' },
         { name: 'za', voice: 'Ayanda', engine: 'neural' },
         { name: 'en', voice: 'Joanna', engine: 'neural' },
+        { name: 'enk', voice: 'Ivy', engine: 'neural' },
         { name: 'enm', voice: 'Matthew', engine: 'neural' },
+        { name: 'enmk', voice: 'Kevin', engine: 'neural' },
         { name: 'fr', voice: 'Léa', engine: 'neural' },
         { name: 'it', voice: 'Bianca', engine: 'neural' },
         { name: 'jpm', voice: 'Takumi', engine: 'neural' },
@@ -56,9 +59,29 @@ methods.getPolly = async function getPolly(client, message, text) {
         region: 'us-east-1',
     })
 
+    var engine = people[0].engine;
+
+
+
+    if (typeSpeak == 'news') {
+        splited = splited.filter(function (item) {
+            return item !== typeSpeak
+        }, language)
+        speakText = `<amazon:domain name="news">  ${splited.join(' ')} </amazon:domain>`
+    } else if (typeSpeak == 'real') {
+        splited = splited.filter(function (item) {
+            return item !== typeSpeak
+        }, language)
+        speakText = `<amazon:auto-breaths volume="x-soft">  ${splited.join(' ')} </amazon:auto-breaths>`
+        engine = 'standard'
+    } else {
+        speakText = replaceAll(splited.join(' '), '##', ' <break time="1s"/> ')
+    }
+
+
     let params = {
-        'Engine': people[0].engine,
-        'Text': '<speak> ' + replaceAll(splited.join(' '),'##',' <break time="1s"/> ') + '</speak>',
+        'Engine': engine,
+        'Text': '<speak> ' + speakText + '</speak>',
         'OutputFormat': 'mp3',
         'VoiceId': people[0].voice,
         'TextType': 'ssml'
@@ -67,7 +90,7 @@ methods.getPolly = async function getPolly(client, message, text) {
     await Polly.synthesizeSpeech(params, (err, data) => {
         if (err) {
             console.log(err.code)
-            Sender.sendMessage(client,message,'Ocorreu um erro! verifique o que você digitou e tente novamente!\n\n' + err,'*ERRO*')
+            Sender.sendMessage(client, message, 'Ocorreu um erro! verifique o que você digitou e tente novamente!\n\n' + err, '*ERRO*')
         } else if (data) {
             if (data.AudioStream instanceof Buffer) {
                 fs.writeFile("Modules/audios/audio.mp3", data.AudioStream, function (err) {
@@ -85,7 +108,7 @@ methods.getPolly = async function getPolly(client, message, text) {
 
 function replaceAll(str, find, replace) {
     return str.replace(new RegExp(find, 'g'), replace);
-  }
+}
 
 methods.getSpeech = async function getSpeech() {
 
